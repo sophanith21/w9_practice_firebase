c882a1859f1c26f2949942dbe57a789d2273255c
Meas Sophanith <sophanithmeas91@gmail.com>
2026-03-22 19:39:10 +0700

    first commit

---
+++ lib/data/repositories/songs/song_repository_firebase.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../../model/songs/song.dart';
+import '../../dtos/song_dto.dart';
+import 'song_repository.dart';
+
+class SongRepositoryFirebase extends SongRepository {
+  final Uri songsUri = Uri.https(
+    'fir-practice-dec8b-default-rtdb.asia-southeast1.firebasedatabase.app',
+    '/songs.json',
+  );
+
+  @override
+  Future<List<Song>> fetchSongs() async {
+    final http.Response response = await http.get(songsUri);
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> songsJson = json.decode(response.body);
+      List<Song> result = [];
+      for (var songId in songsJson.keys) {
+        result.add(SongDto.fromJson(songId, songsJson[songId]));
+      }
+      return result;
+    } else {
+      // 2- Throw expcetion if any issue
+      throw Exception('Failed to load posts');
+    }
+  }
+
+  @override
+  Future<Song?> fetchSongById(String id) async {}
+}
